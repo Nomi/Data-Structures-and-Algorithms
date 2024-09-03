4636cfca045f4ab27bf5cdc065efd5d61fd4affe
Noman <baba.noman@live.com>
2024-09-03 23:14:23 +0000

    A2M8 : NC150 : 2Ptrs : Two Sum II Input Array Is Sorted : LC167 : Submission 1 [Wrong Answer]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

---
+++ NeetCode150/Topics/T02_TwoPointers/P02_TwoSum2InputArrayIsSorted_LC167/TwoSumIiInputArrayIsSorted.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DSA.NeetCode150.Topics.T02_TwoPointers.P02_TwoSum2InputArrayIsSorted_LC167;
+
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        int l=0;
+        int r=numbers.Count()-1;
+        while(true)
+        {
+            var sum = numbers[l]+numbers[r];
+            if(target>sum)
+                l++;
+            else if(target<sum)
+                r--;
+            else //==
+                return new int[2]{l,r};
+        }
+        //GIVEN: There will always be exactly one valid solution.
+        //So we don't need to bother considering quitting out of the loop and stuff.
+    }
+}
