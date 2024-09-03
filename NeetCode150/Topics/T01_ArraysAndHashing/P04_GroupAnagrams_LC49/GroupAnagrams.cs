06aa8340ce12d3b8a4e77598ffb7bfa7b4722ce8
Noman <baba.noman@live.com>
2024-09-03 17:52:13 +0000

    A2M8 : NC150 : A&H : Group Anagrams : LC49 : Submission 2 [Accepted]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T01_ArraysAndHashing/P04_GroupAnagrams_LC49/GroupAnagrams.cs
+++ NeetCode150/Topics/T01_ArraysAndHashing/P04_GroupAnagrams_LC49/GroupAnagrams.cs
@@ -7,13 +7,25 @@ using System.Text;
 namespace DSA.NeetCode150.Topics.T01_ArraysAndHashing.P04_GroupAnagrams_LC49;
 
 public class Solution {
+    // FOR LOWER COMPLEXITY, WE AVOID SORT AND STORE COUNT OF EACH LETTER (joined by ',') AS THE KEY!
     public List<List<string>> GroupAnagrams(string[] strs) {
         Dictionary<string, List<string>> map = new(); //we try to store sorted string as the key and the list stores that specific anagram instance
         foreach(var str in strs)
         {
-            var srtdStr = String.Concat(str.OrderBy(c => c));
-            map.TryAdd(srtdStr, new List<string>());
-            map[srtdStr].Add(str);
+            //------------ NO SORT VERSION -------
+            int[] count = new int[26]; //because there are 26 letters in the English alphabet!
+            foreach(char c in str)
+            {
+                count[c-'a']++;
+            }
+            var key = string.Join(',',count);
+            map.TryAdd(key,new());
+            map[key].Add(str);
+
+            //------- SORT VERSION: --------
+            // var srtdStr = String.Concat(str.OrderBy(c => c)); //nlog(n) where n is AVERAGE length of the strings.
+            // map.TryAdd(srtdStr, new List<string>());
+            // map[srtdStr].Add(str);
         }
         return map.Values.ToList();
     }
