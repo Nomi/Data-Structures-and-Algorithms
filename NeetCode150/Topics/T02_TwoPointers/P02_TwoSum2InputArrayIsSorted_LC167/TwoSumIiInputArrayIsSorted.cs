e43be3894270da5f57726ae33ef71fcb6762bec5
Noman <baba.noman@live.com>
2024-09-03 23:15:06 +0000

    A2M8 : NC150 : 2Ptrs : Two Sum II Input Array Is Sorted : LC167 : Submission 2 [Accepted]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T02_TwoPointers/P02_TwoSum2InputArrayIsSorted_LC167/TwoSumIiInputArrayIsSorted.cs
+++ NeetCode150/Topics/T02_TwoPointers/P02_TwoSum2InputArrayIsSorted_LC167/TwoSumIiInputArrayIsSorted.cs
@@ -18,7 +18,7 @@ public class Solution {
             else if(target<sum)
                 r--;
             else //==
-                return new int[2]{l,r};
+                return new int[2]{1+l,1+r}; //1-indexed
         }
         //GIVEN: There will always be exactly one valid solution.
         //So we don't need to bother considering quitting out of the loop and stuff.
