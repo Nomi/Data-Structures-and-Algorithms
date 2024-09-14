b0f78bb53d17ab4ba43e434acda9f951a378c0b2
Noman <baba.noman@live.com>
2024-09-14 18:11:20 +0000

    A2M8 : NC150 : A&H : Product of Array Except Self : LC238 : Submission 2 [Accepted]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T01_ArraysAndHashing/P07_ProductOfArrayExceptSelf_LC238/ProductOfArrayExceptSelf.cs
+++ NeetCode150/Topics/T01_ArraysAndHashing/P07_ProductOfArrayExceptSelf_LC238/ProductOfArrayExceptSelf.cs
@@ -7,7 +7,14 @@ using System.Text;
 namespace DSA.NeetCode150.Topics.T01_ArraysAndHashing.P07_ProductOfArrayExceptSelf_LC238;
 
 public class Solution {
+    // We are given Each product is guaranteed to fit in a 32-bit integer.
+    //BUT it might serve as a GREAT CLARIFYING QUESTION!!
     public int[] ProductExceptSelf(int[] nums) {
+        // return attempt1(nums);
+        return attempt2(nums);
+    }
+
+    public int[] attempt1(int[] nums) {
         int[] output = new int[nums.Count()];
         int product = 1; //from right to here
         for(int i=nums.Count()-2;i>=0;i--)//-2 cuz we don't need the product there.
@@ -23,5 +30,26 @@ public class Solution {
         }
         output[nums.Count()-1]=product;
         return output;
+    }
+
+
+    public int[] attempt2(int[] nums)
+    {
+        var res = new int[nums.Count()];
+
+        res[0]=1;
+        for(int i=1;i<nums.Count();i++) //we fill result array with product of nums from L.H.S (except itself)
+        {
+            res[i]=res[i-1]*nums[i-1];
+        }
+
+        int product = 1;
+        for(int i=nums.Count()-1;i>=0;i--) //WE NEED PRODUCT CUZ WE AREN'T STORING THIS PRODUCT ANYWHERE (theoretically we could use nums array for it)
+        {
+            res[i]*=product;
+            product*=nums[i];
+        }
+        
+        return res;
     }
 }
