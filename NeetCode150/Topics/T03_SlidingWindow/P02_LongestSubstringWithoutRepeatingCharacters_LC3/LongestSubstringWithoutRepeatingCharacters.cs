e5a20b623e79067de74268905dd1b23105ee5582
Noman <baba.noman@live.com>
2024-09-15 21:11:53 +0000

    A2M8 : NC150 : SW : Longest Substring Without Repeating Characters : LC3 : Submission 6 [Compilation Error]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T03_SlidingWindow/P02_LongestSubstringWithoutRepeatingCharacters_LC3/LongestSubstringWithoutRepeatingCharacters.cs
+++ NeetCode150/Topics/T03_SlidingWindow/P02_LongestSubstringWithoutRepeatingCharacters_LC3/LongestSubstringWithoutRepeatingCharacters.cs
@@ -11,6 +11,20 @@ public class Solution {
     //      TC: O(N)
     //      SC: O(N) (or O(k) where k is the length of longest substring, which is <=N and as such this is asymptotically bounded by O(N))
     public int LengthOfLongestSubstring(string s) {
+        ///READ COMMENTS FROM THIS FOR TIPS IN GENERAL 
+        ///WITH WHILE LOOPS AND SLIDING WINDOW/TWO POINTERS!:
+        // return attempt1(s);
+
+        //USE FOR MAIN REFERENCE:
+        return NeetCodeProvidedSoln(s);
+
+
+        //PRACTICE ATTEMPTS:
+        //return attempt2(string s);
+    }
+
+    public int attempt1(string s)
+    {
         if(s.Length<2)
             return s.Length;
         HashSet<char> hs = new();
@@ -19,6 +33,11 @@ public class Solution {
         int maxLength=1;
         while(r<s.Length)
         {
+            // THIS WAS HERE EARLIER BUT THAT'S DUMB AND BAD
+            // because it only updates max if there's a duplicate
+            // which won't trigger if the string reaches the end 
+            // for the last substring.
+            // maxLength=(int)Math.Max(r-l+1, maxLength);
             if(hs.Contains(s[r]))
             {
                 while(hs.Contains(s[r]))
@@ -34,9 +53,34 @@ public class Solution {
                 }
             }
             hs.Add(s[r]);
+            
+            //THIS NEEDS TO BE HERE AND NOT ABOVE (like I had it earlier)
+            //AS EXPLAINED ABOVE!
             maxLength=(int)Math.Max(r-l+1, maxLength);
             r++;
         }
         return maxLength;
+    }
+
+    public int NeetCodeProvidedSoln(string s)
+    {
+                HashSet<char> charSet = new HashSet<char>();
+        int l = 0;
+        int res = 0;
+
+        for (int r = 0; r < s.Length; r++) {
+            while (charSet.Contains(s[r])) {
+                charSet.Remove(s[l]);
+                l++;
+            }
+            charSet.Add(s[r]);
+            res = Math.Max(res, r - l + 1);
+        }
+        return res;
+    }
+
+    public int attempt1(string s)
+    {
+
     }
 }
