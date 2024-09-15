9b0ad1c40a8581589ef4c69f5d42edce5be06d1b
Noman <baba.noman@live.com>
2024-09-15 18:04:54 +0000

    A2M8 : NC150 : 2Ptrs : 3Sum : LC15 : Submission 4 [Accepted]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T02_TwoPointers/P03_3sum_LC15/3sum.cs
+++ NeetCode150/Topics/T02_TwoPointers/P03_3sum_LC15/3sum.cs
@@ -8,12 +8,17 @@ namespace DSA.NeetCode150.Topics.T02_TwoPointers.P03_3sum_LC15;
 
 public class Solution {
     public List<List<int>> ThreeSum(int[] nums) {
-        // return attempt1(nums);
-        return NeetCodeSolnBasedAttempt(nums);
+        //READ THIS: https://leetcode.com/problems/3sum/editorial/comments/1056876
+        // return attemptNoSort1(nums);
+
+        //Main Way to Solve:
+        // return attemptTwoPtr1_NeetCodeSolnBasedAttempt(nums);
+        return attemptTwoPtr2(nums);
     }
 
-    //TC: O(nlog(n))
-    public List<List<int>> NeetCodeSolnBasedAttempt(int[] nums) 
+    //TC: O(n^2) // O(nlog(n)+n^2) but n^2 dominates nlog(n) asympotitcally
+    //SC: O(1)
+    public List<List<int>> attemptTwoPtr1_NeetCodeSolnBasedAttempt(int[] nums) 
     {
         List<List<int>> res = new();
         Array.Sort(nums);//TC: O(nlog(n))
@@ -61,26 +66,66 @@ public class Solution {
         return res;
     }
 
-    public List<List<int>> attempt1(int[] nums) 
+    //TC: O(n^2)
+    //SC: O(n)
+    //This is only better when you're not allowed to change the nums array
+    //as then you'd have to store a copy of nums array as well, 
+    //at which point it'd be better to just use this method where you keep a hashset to keep track of duplicates partially.
+    public List<List<int>> attemptNoSort1(int[] nums) 
     {
         List<List<int>> res = new();
-        // HashSet<string> used = new();
-        for(int i=0;i<nums.Count();i++)
+        HashSet<string> used = new(); //could've used tuple?
+        for(int i=0;i<nums.Count();i++) //TC: O(n^2)
         {
             HashSet<int> hs = new();
             int subSumTarget = -nums[i]; //target for the other two numbers.
-            for(int j=i+1;j<nums.Count();j++)
+            for(int j=i+1;j<nums.Count();j++) //this is pretty much twosum 2
             {
                 if(hs.Contains(subSumTarget-nums[j])) //the only times duplicate numbers matter is when they're both the solution, which this condition helps with to make sure that the hashset doesn't affect our ability to handle that case by having the check be here.
                 {
                     List<int> curr = new(){nums[i],subSumTarget-nums[j],nums[j]};
-                    // string str = string.Join(',',curr.OrderBy(x=>x).ToList());
-                    // if(used.Contains(str))
-                    //     continue;
-                    // used.Add(str);
+
+                    //Could've been used tuples here (instead of str)!
+                    string str = string.Join(',',curr.OrderBy(x=>x).ToList()); //This sort is constant time because there's always 3 comparisons!
+                    if(used.Contains(str))
+                        continue;
+                    used.Add(str);
                     res.Add(curr);
                 }
                 hs.Add(nums[j]);
+            }
+        }
+        return res;
+    }
+
+    public List<List<int>> attemptTwoPtr2(int[] nums)
+    {
+        List<List<int>> res = new();
+        //For avoiding duplicates, this sorting is better because of space complexity being O(1).
+        Array.Sort(nums);
+        for(int i=0; i<nums.Count();i++)
+        {
+            if(i>0&&nums[i]==nums[i-1])
+                continue; //Avoiding duplicates (cuz array sorted)
+            int target = -nums[i];
+            int l=i+1, r=nums.Count()-1;
+            while(l<r)
+            {
+                var sum = nums[l]+nums[r];
+                if(sum>target)
+                    r--;
+                else if(sum<target)
+                    l++;
+                else //==
+                {
+                    res.Add(new(){nums[i],nums[l],nums[r]});
+                    l++;
+                    r--;
+                    while(l<r&&nums[r]==nums[r+1])
+                        r--;
+                    while(l<r&&nums[l]==nums[l-1])
+                        l++;
+                }
             }
         }
         return res;
