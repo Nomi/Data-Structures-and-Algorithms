2300fc203b0f42658b8bcfdcb1b6993eacc7b537
Noman <baba.noman@live.com>
2024-09-15 21:00:49 +0000

    A2M8 : NC150 : SW : Longest Substring Without Repeating Characters : LC3 : Submission 2 [Wrong Answer]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T03_SlidingWindow/P02_LongestSubstringWithoutRepeatingCharacters_LC3/LongestSubstringWithoutRepeatingCharacters.cs
+++ NeetCode150/Topics/T03_SlidingWindow/P02_LongestSubstringWithoutRepeatingCharacters_LC3/LongestSubstringWithoutRepeatingCharacters.cs
@@ -7,9 +7,9 @@ using System.Text;
 namespace DSA.NeetCode150.Topics.T03_SlidingWindow.P02_LongestSubstringWithoutRepeatingCharacters_LC3;
 
 public class Solution {
-    //
-    //TC: O(N)
-    //SC: O(N) [or O(k) where k is the length of the longest substring, which can be at most O(N) and as such, is asymptotically bounded by that]
+    //# Sliding Window:
+    //      TC: O(N)
+    //      SC: O(N) [or O(k) where k is the length of longest substring, which is <=N and as such this is asymptotically bounded by O(N)]
     public int LengthOfLongestSubstring(string s) {
         HashSet<char> hs = new();
         int l=0;int r=1;
