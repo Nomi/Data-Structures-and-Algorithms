0a8fdd8cafd19691bf41e354db1c47004cfca3ac
Noman <baba.noman@live.com>
2024-09-16 22:33:50 +0000

    A2M8 : NC150 : Stack : Min Stack : LC155 : Submission 2 [Accepted]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T04_Stack/P02_MinStack_LC155/MinStack.cs
+++ NeetCode150/Topics/T04_Stack/P02_MinStack_LC155/MinStack.cs
@@ -10,31 +10,42 @@ public class MinStack {
     //THESE PROVIDED CONSTRAINTS MIGHT SERVE AS GOOD CLARIFYING QUESTIONS???
     //-2^31 <= val <= 2^31 - 1. //fits in int32?
     //pop, top and getMin will always be called on non-empty stacks.
-    LinkedList<int> values;
-    LinkedList<int> minimums;
+    
+
+    //Solution: We clearly just use another place that keeps
+    // track of the minimum at each level of insertions,
+    // so that when Pop is done, instead of recalculating
+    // we can just remove the last value from both values
+    // and minumum and have the last elements be the up to date
+    // without having to recalculate maximum.
+    
+    // LinkedList<int> values;
+    // LinkedList<int> minimums;
+    Stack<int> values;
+    Stack<int> minimums;
     public MinStack() {
         values = new();
-        minimums=new();
+        minimums = new();
     }
     
     public void Push(int val) {
-        values.AddLast(val);
+        values.Push(val);
         if(minimums.Count==0)
-            minimums.AddLast(val);
+            minimums.Push(val);
         else
-            minimums.AddLast((int)Math.Min(val,minimums.Last.Value));
+            minimums.Push((int)Math.Min(val,minimums.Peek()));
     }
     
     public void Pop() {
-        values.RemoveLast();
-        minimums.RemoveLast();
+        values.Pop();
+        minimums.Pop();
     }
     
     public int Top() {
-        return values.Last.Value;
+        return values.Peek();
     }
     
     public int GetMin() {
-        return minimums.Last.Value;
+        return minimums.Peek();
     }
 }
