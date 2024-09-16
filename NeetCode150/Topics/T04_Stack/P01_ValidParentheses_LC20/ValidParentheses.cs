a7e39611e7afc5290906ec03c0030730c17a895a
Noman <baba.noman@live.com>
2024-09-16 22:15:43 +0000

    A2M8 : NC150 : Stack : Valid Parentheses : LC20 : Submission 3 [Compilation Error]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T04_Stack/P01_ValidParentheses_LC20/ValidParentheses.cs
+++ NeetCode150/Topics/T04_Stack/P01_ValidParentheses_LC20/ValidParentheses.cs
@@ -8,10 +8,44 @@ namespace DSA.NeetCode150.Topics.T04_Stack.P01_ValidParentheses_LC20;
 
 public class Solution {
     public bool IsValid(string s) {
-        return attempt1(s);
+        //USE NEETCODE BASED ATTEMPT FOR ACTUAL REFERENCE!!!
+        // return attempt1(s);
+
+        //USE THIS FOR ACTUAL REFERENCE:
+        return NeetCodeBasedSoln(s); // (also, uses the nifty trick of using the HashMap closingToOpen instead of openingToClosing)
     }
     
-    public bool attempt1(string s)
+    //USE THIS FOR ACTUAL REFERENCE:
+    // (also, uses the nifty trick of using the HashMap
+    // closingToOpen instead of openingToClosing)
+    public bool NeetCodeBasedSoln(string s) 
+    {
+        Stack<char> stack = new();
+        Dictionary<char,char> closeToOpen = new(){
+            {')','('},
+            {'}','{'},
+            {'}','{'}};
+
+        foreach(char c in s)
+        {
+            if(!closeToOpen.ContainsKey(c)) //not closing bracket //I guess we don't need to consider wrong input otherwise the hashmap would break on using them as key if not checked for containing it as key
+            {
+                //by if: it is not a closing bracket
+                stack.Push(c);
+            }
+            else if(stack.Count==0 || stack.Pop() != pairs[c])
+            {
+                //by else: it is a closing bracket
+                //by if: 
+                //      1. there were no opening brackets before this.
+                //      2. the last opening bracket was of a different type than this closing bracket.
+                return false;
+            }
+        }
+        return (0==stack.Count);
+    }
+
+    public bool attempt1(string s) //USE NEETCODE BASED ATTEMPT FOR ACTUAL REFERENCE
     {
         Stack<char> stack = new();
         Dictionary<char,char> openToClose = new(){
@@ -31,7 +65,7 @@ public class Solution {
                 continue;
             }
             char lastChar = stack.Pop();
-            if(!openToClose.ContainsKey(lastChar))
+            if(!openToClose.ContainsKey(lastChar)) //I guess we don't need this and we can assume it input will always contain only the provided chars???
                 return false;
             if(c!=openToClose[lastChar])
                 return false;
