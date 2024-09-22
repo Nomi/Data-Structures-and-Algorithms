2345bfb0f845b3023790e32e095ad33e2a3bb13e
Noman <baba.noman@live.com>
2024-09-22 16:32:14 +0000

    A2M8 : NC150 : BS : Time Based Key Value Store : LC981 : Submission 2 [Accepted]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T05_BinarySearch/P06_TimeBasedKeyValueStore_LC981/TimeBasedKeyValueStore.cs
+++ NeetCode150/Topics/T05_BinarySearch/P06_TimeBasedKeyValueStore_LC981/TimeBasedKeyValueStore.cs
@@ -38,6 +38,14 @@ public class TimeMap {
                 //we don't need Math.Max because any further 
                 //ones matching this condition will be the 
                 //most recent because we increase l.
+
+                //e.g. considering only timestamps: [0,1,2,4,6] 
+                //At timestamp 5 as input, we use bs:
+                //first check mid at m=2, which is 2 and 2<=5
+                //so either it is the newest timestamp that we can consider
+                //or there's one or more after it <=5. We don't consider anything to its left including itself (by setting left to mid+1) because we already know there's one <=5 that's newer than those (as for 2 itself, we already set it as the newest and it won't change unless we find a newer timestamp <=5)
+                //Then m=3, which is 4, and just like above, we set timestamp to this because it is newer than all those considered before it (by definition).
+                //Then m = 4, which is 6, but 6 is > 5, so we don't consider and move r to mid-1.
             }
         }
         return res;
