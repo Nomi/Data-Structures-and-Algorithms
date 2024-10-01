379ad9dbf2ea8558c024e38687b73d4403e4c83c
Noman <baba.noman@live.com>
2024-10-01 20:32:48 +0000

    A2M8 : NC150 : Trees : Invert Binary Tree : LC226 : Submission 4 [Accepted]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T07_Trees/P01_InvertBinaryTree_LC226/InvertBinaryTree.cs
+++ NeetCode150/Topics/T07_Trees/P01_InvertBinaryTree_LC226/InvertBinaryTree.cs
@@ -25,6 +25,7 @@ namespace DSA.NeetCode150.Topics.T07_Trees.P01_InvertBinaryTree_LC226;
 public class Solution {
     public TreeNode InvertTree(TreeNode root) {
         //REMEMBER TO CHECK FOR NULL (leafs)!!!
+        //RECURSIVE SOLUTION BETTER??? (No reason except that the Neetcode solution uses that.)
         // return iterative1(root);
         return recursive1(root);
     }
@@ -54,7 +55,7 @@ public class Solution {
             return null;
 
         var tmp = root.left;
-        
+
         root.left = recursive1(root.right);
         root.right = recursive1(tmp);
 
