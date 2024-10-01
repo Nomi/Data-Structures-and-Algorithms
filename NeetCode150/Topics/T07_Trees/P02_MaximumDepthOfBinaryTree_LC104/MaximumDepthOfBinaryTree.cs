0c3e4df518083de8dac7d6b9af00a919c7cdf49b
Noman <baba.noman@live.com>
2024-10-01 22:51:47 +0000

    A2M8 : NC150 : Trees : Maximum Depth of Binary Tree : LC104 : Submission 2 [Accepted]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T07_Trees/P02_MaximumDepthOfBinaryTree_LC104/MaximumDepthOfBinaryTree.cs
+++ NeetCode150/Topics/T07_Trees/P02_MaximumDepthOfBinaryTree_LC104/MaximumDepthOfBinaryTree.cs
@@ -24,13 +24,38 @@ namespace DSA.NeetCode150.Topics.T07_Trees.P02_MaximumDepthOfBinaryTree_LC104;
 
 public class Solution {
     public int MaxDepth(TreeNode root) {
-        return rec1(root);
+        // return recDfs1(root); //Best solution?
+        return itrBfs1(root);
     }
 
-    public int rec1(TreeNode root)
+    public int recDfs1(TreeNode root) //Best solution?
     {
         if(root == null)
             return 0;
-        return (int) Math.Max(1+rec1(root.left), 1+rec1(root.right));
+        return (int) Math.Max(1+recDfs1(root.left), 1+recDfs1(root.right));
+    }
+
+    public int itrBfs1(TreeNode root) 
+    {
+        if(root==null)
+            return 0;
+        Queue<TreeNode> q = new();
+        q.Enqueue(root);
+        int maxDepth = 0;
+
+        while(q.Count>0)
+        {
+            maxDepth++;
+            var countAtThisDepth = q.Count;
+            for(int i=0;i<countAtThisDepth;i++)
+            {
+                var cur = q.Dequeue();
+                if(cur.left!=null)
+                    q.Enqueue(cur.left);
+                if(cur.right!=null)
+                    q.Enqueue(cur.right);
+            }
+        }
+        return maxDepth;
     }
 }
