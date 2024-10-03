268f549c05ad5e65ad0af9bb2729ea86b484ce85
Noman <baba.noman@live.com>
2024-10-03 22:28:37 +0000

    A2M8 : NC150 : Trees : Subtree of Another Tree : LC572 : Submission 2 [Accepted]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T07_Trees/P06_SubtreeOfAnotherTree_LC572/SubtreeOfAnotherTree.cs
+++ NeetCode150/Topics/T07_Trees/P06_SubtreeOfAnotherTree_LC572/SubtreeOfAnotherTree.cs
@@ -24,17 +24,50 @@ namespace DSA.NeetCode150.Topics.T07_Trees.P06_SubtreeOfAnotherTree_LC572;
 
 public class Solution {    
     public bool IsSubtree(TreeNode root, TreeNode subRoot) {
-        return Attempt1_MyOwn.rec1(root, subRoot);
+        //USE THE NEETCODE SOLN AS REFERENCE!!! (compare to mine?)
+
+        // return Attempt1_MyOwn.rec1(root, subRoot);
+        return NeetCodeIoBasedSoln.IsSubtree(root, subRoot);
+    }
+}
+
+public static class NeetCodeIoBasedSoln
+{
+    public static bool IsSubtree(TreeNode root, TreeNode subRoot) { //Recursive function
+        //EDGECASE - COULDN'T HAVE THOUGHT OF THIS: EMPTY TREE IS SUBTREE OF EVERY TREE.
+        if (subRoot == null)
+            return true;
+        //ELSE (because of the return in the if)
+        if (root == null) //we know root is not the same as subRoot when its null because of the if above.
+            return false;
+        //ELSE (because of the return in the if)
+        if (SameTree(root, subRoot))  //recursive function used inside another recursive function!
+        {
+            return true;
+        }
+        //ELSE (because of the return in the if)
+        return IsSubtree(root.left, subRoot) || IsSubtree(root.right, subRoot);
+    }
+
+    public static bool SameTree(TreeNode root, TreeNode subRoot) { //Recursive function
+        //My attempt for this part was a little bit better so I used that!
+        if(root == null && subRoot == null)
+            return true;
+        
+        if(root?.val != subRoot?.val)
+            return false;
+
+        return SameTree(root.left, subRoot.left) && SameTree(root.right, subRoot.right);
     }
 }
 
 public static class Attempt1_MyOwn
 {
-    public static bool rec1(TreeNode root, TreeNode subRoot)
+    public static bool rec1(TreeNode root, TreeNode subRoot) 
     {
         bool found = false;
         if(root?.val==subRoot?.val)
-            found = recEqCheck1(root, subRoot);
+            found = recEqCheck1(root, subRoot); //recursive function used inside another recursive function!
         else if(root==null) //above condition implies subRoot != null while here root is. Therefore, we'll be unable to find the subtree from here.
             return false;
         if(found)
@@ -46,9 +79,9 @@ public static class Attempt1_MyOwn
         }
         return false;
     }
-    public static bool recEqCheck1(TreeNode root, TreeNode subRoot)
+    public static bool recEqCheck1(TreeNode root, TreeNode subRoot) //Recursive function
     {
-        Console.WriteLine($"{root?.val}, {subRoot?.val}");
+        // Console.WriteLine($"{root?.val}, {subRoot?.val}");
         if(root == null && subRoot == null)
             return true;
         
