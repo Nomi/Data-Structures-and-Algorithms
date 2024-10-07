cd634d61572001464fa233e8c36faa14085775b7
Noman <baba.noman@live.com>
2024-10-07 20:11:12 +0000

    A2M8 : NC150 : Trees : Lowest Common Ancestor of a Binary Search Tree : LC235 : Submission 6 [Accepted]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T07_Trees/P07_LowestCommonAncestorOfABinarySearchTree_LC235/LowestCommonAncestorOfABinarySearchTree.cs
+++ NeetCode150/Topics/T07_Trees/P07_LowestCommonAncestorOfABinarySearchTree_LC235/LowestCommonAncestorOfABinarySearchTree.cs
@@ -25,22 +25,43 @@ namespace DSA.NeetCode150.Topics.T07_Trees.P07_LowestCommonAncestorOfABinarySear
 public class Solution {
     public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 
-        //READ THE COMMENTS FOR ATTEMPT1!!!
-        var bigger = p.val>=q.val ? p : q;
-        var smaller = p.val>=q.val ? q : p;
-        return rec1(root, smaller, bigger);
+        //IMPORTANT! USE NEETCODEIO ITERATIVE SOLUTION 
+        // BECAUSE IT IS BETTER DUE TO SAME TIME COMPLEXITY BUT O(1) SPACE COMPLEXITY 
+        // (recursive has O(log(n) space complexity)!!!!
+        //READ THE COMMENTS FOR rec1 (and rec1Helper) 
+        return rec1(root, p, q);
     }
 
-    public TreeNode rec1(TreeNode root, TreeNode smaller, TreeNode bigger)
+    public TreeNode NeetCodeBasedSoln(TreeNode root, TreeNode p, TreeNode q) //Should modify to bigger and smaller precalculated like in rec1 to simplify conditions!
+    {
+        while(true)
+        {
+            if(root.val < p.val && root.val < q.val) //Checking both here because we don't know which one is bigger or smaller, but we could modify it that way (like in the recursive solution!)
+                root = root.right;
+            else if (root.val > p.val && root.val > q.val)
+                root = root.left;
+            else
+                return root; // Check comments in rec1Helper to check why this works!
+        }
+    }
+    //TC: O(log(n))
+    //SC: O(log(n))
+    public TreeNode rec1(TreeNode root, TreeNode p, TreeNode q)
+    {
+        var bigger = p.val>=q.val ? p : q;
+        var smaller = p.val>=q.val ? q : p;
+        return rec1Helper(root, smaller, bigger);
+    }
+    public TreeNode rec1Helper(TreeNode root, TreeNode smaller, TreeNode bigger)
     {
         // Console.WriteLine($"{root?.val} : {smaller.val}, {bigger.val}");
 
         if(root.val>bigger.val)
-            return rec1(root.left, smaller, bigger);
+            return rec1Helper(root.left, smaller, bigger);
         if(root.val<smaller.val)
-            return rec1(root.right, smaller, bigger);
+            return rec1Helper(root.right, smaller, bigger);
 
-        //IF root.val >= smaller.val || root.val <= bigger.val (because of above if conditions) 
+        //IF root.val >= smaller.val && root.val <= bigger.val (because of above if conditions) 
         
         //Notice how because of the conditions above we will only ever get here when in the following cases:
         //
