a22b895a3777af04e6b44a71ea1a29f5d8be8e66
Noman <baba.noman@live.com>
2024-10-07 22:43:08 +0000

    A2M8 : NC150 : Trees : Count Good Nodes In Binary Tree : LC1448 : Submission 2 [Accepted]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T07_Trees/P10_CountGoodNodesInBinaryTree_LC1448/CountGoodNodesInBinaryTree.cs
+++ NeetCode150/Topics/T07_Trees/P10_CountGoodNodesInBinaryTree_LC1448/CountGoodNodesInBinaryTree.cs
@@ -26,7 +26,8 @@ public class Solution {
     public int GoodNodes(TreeNode root) {
         return rec1Dfs(root, int.MinValue);
     }
-
+    //TC: O(2^n)
+    //SC: O(2^n)
     public int rec1Dfs(TreeNode root, int maxThusFar)
     {
         if(root == null)
