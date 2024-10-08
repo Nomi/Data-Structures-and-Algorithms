e0546a46ea3ddf8cf57a5cdce41cd76e5353019e
Noman <baba.noman@live.com>
2024-10-08 21:05:13 +0000

    A2M8 : NC150 : Trees : Validate Binary Search Tree : LC98 : Submission 2 [Accepted]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T07_Trees/P11_ValidateBinarySearchTree_LC98/ValidateBinarySearchTree.cs
+++ NeetCode150/Topics/T07_Trees/P11_ValidateBinarySearchTree_LC98/ValidateBinarySearchTree.cs
@@ -24,18 +24,29 @@ namespace DSA.NeetCode150.Topics.T07_Trees.P11_ValidateBinarySearchTree_LC98;
 
 public class Solution {
     public bool IsValidBST(TreeNode root) {
-        return rec1Dfs(root);    
+        //EASY BUT TRICKY (Important: read comments on the `rec1Dfs` function.)
+        //COULDN'T COME UP WITH MY OWN SOLUTION AND HAND TO WATCH THE FIRST ~5 MINUTES OF Greg Hogg's VIDEO ABOUT IT TO COME UP WITH THE CODE (the basic idea was from the video itself, obviously)
+        return rec1Dfs(root, int.MinValue, int.MaxValue); // Given the constraints, could've also done: -1001, 1001);
     }
 
     //TC: O(N)
     //SC: O(N) [worst case, best case log2(n)]
-    public bool rec1Dfs(TreeNode root)
+    public bool rec1Dfs(TreeNode root, int min, int max) //Clearly in-order is more efficient because it ends quickly.
     {
+        //Base case:
         if(root==null)
             return true;
 
-        bool isCurNodeValid = (root.left==null||root.left.val<root.val) && (root.right==null||root.right.val>root.val);
+        //The exclusive range (min, max) is such that: 
+        //- `min` is the value of the most recent node from which the direction of our traversal was to the right (meaning all elements
+        //from the subtree starting at that right child had to be bigger than that).
+        //- `max` is the value we have seen at the most recent node from which we travel left (meaning all elements of the subtree starting
+        //at the left child are smaller than that).
+        bool isCurNodeValid = (min < root.val) && (root.val < max); //min and max are exclusive because of how it's implemented.
 
-        return isCurNodeValid && rec1Dfs(root.left) && rec1Dfs(root.right);
+        return isCurNodeValid &&  
+            rec1Dfs(root.left, min, root.val) &&  
+            rec1Dfs(root.right, root.val, max);
+        //For an explanation of how/why this works, either read the comments on isCurNodeValid evaluation or watch Greg Hogg's video.
     }
 }
