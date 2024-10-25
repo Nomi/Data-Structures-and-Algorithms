7724047fa90bbffcbd86c26c296e22532cb402b6
Noman <baba.noman@live.com>
2024-10-25 00:54:44 +0000

    A2M8 : NC150 : H/PQ : Find Median From Data Stream : LC295 : Submission 2 [Wrong Answer]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T08_HeapAndPriorityQueue/P07_FindMedianFromDataStream_LC295/FindMedianFromDataStream.cs
+++ NeetCode150/Topics/T08_HeapAndPriorityQueue/P07_FindMedianFromDataStream_LC295/FindMedianFromDataStream.cs
@@ -7,17 +7,17 @@ using System.Text;
 namespace DSA.NeetCode150.Topics.T08_HeapAndPriorityQueue.P07_FindMedianFromDataStream_LC295;
 
 public class MedianFinder {
-
+    IMedianFinder solver;
     public MedianFinder() {
-        
+        solver = new attempt1();
     }
     
     public void AddNum(int num) {
-        
+        solver.AddNum(num);
     }
     
     public double FindMedian() {
-        
+        return solver.FindMedian();
     }
 }
 
@@ -29,21 +29,50 @@ public interface IMedianFinder {
 }
 
 public class attempt1 : IMedianFinder
-{
-    PriorityQueue leftHalf;
-    PriorityQueue rightHalf;
+{   
+    //Space Complexity: O(N)
+    PriorityQueue<int,int> leftHalf; //maxheap
+    PriorityQueue<int,int> rightHalf; //minheap
+    
     public attempt1()
     {
-        
+        leftHalf = new();
+        rightHalf = new();
+        // //Filling with dummy values to make our checks while adding easier (not checking if either are empty)
+        // leftHalf.Enqueue(int.MinValue, int.MaxValue); //priority = int.MaxValue cuz priority queue has smalles priorty first (and -int.MinValue overflows)
+        // rightHalf.Enqueue(int.MaxValue, int.MaxValue); //priority = int.MaxValue cuz priority queue has smalles priorty first
     }
 
-    public void AddNum(int num)
+    //IMPORTANT!!! TAKE A LOOK AT THIS FUNCTION (and how it's done)!!! (I had to take a peek at the NeetCodeIo solution initally a little to get how its done)
+    //Time Complexity: O(nlog(n))
+    public void AddNum(int num) //THIS FUNCTION REQUIRED ME TO TAKE A BRIEF LOOK AT THE NEETCODEIO SOLN!
     {
+        if(rightHalf.Count != 0 && num > rightHalf.Peek()) // !=0 because we try to insert to left first, arbitrarily chosen. Could do right too.
+            rightHalf.Enqueue(num, num); //minheap so we use num as prio
+        else
+            leftHalf.Enqueue(num, -num); //maxheap so we use -num as prio
 
+        //Make sure the 'halves' are balanced:
+        if(leftHalf.Count > rightHalf.Count+1) //+1 for when queue has odd number of elements
+        {
+            int largestOnLeft = leftHalf.Dequeue();
+            rightHalf.Enqueue(largestOnLeft, -largestOnLeft); //maxheap so we use (-largestOnLeft) as priority
+        }
+        else if(rightHalf.Count > leftHalf.Count+1) //+1 for when queue has odd number of elements
+        {
+            int smallestOnRight = rightHalf.Dequeue();
+            leftHalf.Enqueue(smallestOnRight, smallestOnRight); //minheap so we keep smallestOnRight as priority
+        }
     }
 
+    //Time Complexity: O(1)
     public double FindMedian()
     {
-
+        //odd number of numbers:
+        if(leftHalf.Count!=rightHalf.Count) 
+            return leftHalf.Count>rightHalf.Count ? leftHalf.Peek() : rightHalf.Peek();
+        
+        //even number of numbers:
+        return (((double)leftHalf.Peek()+rightHalf.Peek())/2.0);
     }
 }
