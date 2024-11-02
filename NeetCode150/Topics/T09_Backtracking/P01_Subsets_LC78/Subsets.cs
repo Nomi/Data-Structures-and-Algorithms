97f8dedc2ca245bbd4dc29bcb12a8efb241d315b
Noman <baba.noman@live.com>
2024-11-02 22:55:17 +0000

    A2M8 : NC150 : Bktrk : Subsets : LC78 : Submission 2 [Accepted]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T09_Backtracking/P01_Subsets_LC78/Subsets.cs
+++ NeetCode150/Topics/T09_Backtracking/P01_Subsets_LC78/Subsets.cs
@@ -8,12 +8,26 @@ namespace DSA.NeetCode150.Topics.T09_Backtracking.P01_Subsets_LC78;
 
 public class Solution {
     public List<List<int>> Subsets(int[] nums) {
+        List<List<int>> res = new(1<<nums.Length); // 1 << nums.Length == 2^nums.Length (which is number of possible subsets (because there are max n numbers and two choices for each of them: pick it or not))
         
+        backtrack1(
+            nums: nums,
+            subset: new(nums.Length), // Min subset length = 0, Max subset length = nums.Length
+            idx: 0,
+            res: res);
+        
+        return res;
     }
 
-    public List<List<int>> backtrack1(int[] nums)
+    public void backtrack1(int[] nums, int idx, List<int> subset, List<List<int>> res)
     {
-        int numSubsets = 1 << nums.Length; //==2^nums.Length
+        if(idx==nums.Length) //We're at the leaf node (just think about it. Watching neetcodeio video might help visualize!)
+        {
+            res.Add(new(subset)); //We use the copy constructor to avoid messing up the reference
+            return;
+        }
+
+        // int numSubsets = 1 << nums.Length; //==2^nums.Length
         
         // IMPORTANT NOTE: 
         // Min subset length = 0, Max subset length = nums.Length
@@ -24,5 +38,15 @@ public class Solution {
         // (thankfully, the constraints given to us show that the input will be small so it's fine!)
 
         //IMPORTANT NOTE: BACKTRACKING GIVES US ONLY UNIQUE SUBSETS HERE BECAUSE THE INPUT HAS UNIQUE VALUES (no duplicates).
+
+        // CASE 2: WE DO NOT USE THE CURRENT ELEMENT
+        backtrack1(nums, idx+1, subset, res);
+
+        // CASE 2: WE USE THE CURRENT ELEMENT
+        subset.Add(nums[idx]);//Adding current to the end!
+        backtrack1(nums, idx+1,subset, res);
+        subset.RemoveAt(subset.Count - 1); //remove last element. //NEED TO REMOVE THIS BECAUSE IT WILL BE USED ON THE LEVEL ABOVE THIS (cuz it is shared via reference)
+
+        return;
     }
 }
