bce7c5682bc0ce190e5356b0b95564979ba6861a
Noman <baba.noman@live.com>
2024-11-03 04:25:16 +0000

    A2M8 : NC150 : Bktrk : Permutations : LC46 : Submission 1 [Accepted]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

---
+++ NeetCode150/Topics/T09_Backtracking/P03_Permutations_LC46/Permutations.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DSA.NeetCode150.Topics.T09_Backtracking.P03_Permutations_LC46;
+
+public class Solution {
+    public List<List<int>> Permute(int[] nums) {
+        return backtrack1(nums);
+    }
+
+    public List<List<int>> backtrack1(int[] nums) 
+    {
+        List<List<int>> res = new();
+        backtrack1Helper(nums, subset: new(nums.Length), hashset: new(nums.Length), res);
+        return res;
+    }
+
+    public void backtrack1Helper(int[] nums, List<int> subset, HashSet<int> hashset, List<List<int>> res)
+    {
+        if(subset.Count == nums.Length)
+        {
+            res.Add(subset.ToList());
+        }
+
+        for(int i=0; i<nums.Length; i++)
+        {
+            if(hashset.Contains(nums[i]))
+                continue;
+            
+            subset.Add(nums[i]);
+            hashset.Add(nums[i]);
+            backtrack1Helper(nums, subset, hashset, res);
+            subset.RemoveAt(subset.Count-1);
+            hashset.Remove(nums[i]);
+            //Since we MUST pick all of them at least once (by task description), we don't consider the case where we don't pick it, but just the cases for all possible numbers being at this position.
+        }
+    }
+}
