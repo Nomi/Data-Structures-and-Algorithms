aef9236be6ac9cf5c071ec5049e0ece9d40068c4
Noman <baba.noman@live.com>
2024-11-04 01:58:05 +0000

    A2M8 : NC150 : Bktrk : Combination Sum II : LC40 : Submission 1 [Accepted]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

---
+++ NeetCode150/Topics/T09_Backtracking/P05_CombinationSum2_LC40/CombinationSumIi.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DSA.NeetCode150.Topics.T09_Backtracking.P05_CombinationSum2_LC40;
+
+public class Solution {
+    public List<List<int>> CombinationSum2(int[] candidates, int target) {
+        return backtrack1_PickOrNot(candidates, target);
+    }
+
+
+    public List<List<int>> backtrack1_PickOrNot(int[] candidates, int target) 
+    {
+        List<List<int>> res = new();
+        Array.Sort(candidates);
+
+        backtrack1_PickOrNotHelper(
+            candidates,
+            idx: 0,
+            target,
+            curSum: 0,
+            subset: new(candidates.Length),
+            res
+        );
+        return res;
+    }
+
+    public void backtrack1_PickOrNotHelper(int[] candidates, int idx,int target, int curSum, List<int> subset, List<List<int>> res)
+    {
+        if(target == curSum)
+        {
+            res.Add(subset.ToList());
+            return;
+        }
+        if(curSum>target || idx == candidates.Length) //curSum>target => we can't get desired target because all elements >=1 according to constraints.
+            return;
+
+        //Case 1: Pick this
+        subset.Add(candidates[idx]);
+        backtrack1_PickOrNotHelper(candidates, idx+1, target, curSum+candidates[idx], subset, res);
+        subset.RemoveAt(subset.Count-1);
+
+        //Skip repetition of this element (since the above loop takes care of that and we don't want to do it twice(or more)!!!!)
+        while((idx+1)<candidates.Length && candidates[idx]==candidates[(idx+1)])
+            idx++;
+
+        //Case 2: Don't pick this
+        backtrack1_PickOrNotHelper(candidates, idx+1, target, curSum, subset, res);
+        
+        return;
+    }
+}
