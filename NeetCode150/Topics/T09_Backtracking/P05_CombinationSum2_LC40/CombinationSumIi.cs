d97aac7de964ff7ee50f91e72ad7b676703d6844
Noman <baba.noman@live.com>
2024-11-05 02:32:52 +0000

    A2M8 : NC150 : Bktrk : Combination Sum II : LC40 : Submission 8 [Accepted]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T09_Backtracking/P05_CombinationSum2_LC40/CombinationSumIi.cs
+++ NeetCode150/Topics/T09_Backtracking/P05_CombinationSum2_LC40/CombinationSumIi.cs
@@ -9,7 +9,7 @@ namespace DSA.NeetCode150.Topics.T09_Backtracking.P05_CombinationSum2_LC40;
 public class Solution {
     public List<List<int>> CombinationSum2(int[] candidates, int target) {
         //https://algo.monster/flowchart
-        
+
         //CHECK THE HASHMAP AND OPTIMAL VERSIONS ON NEETCODEIO (written ones)?????
         //WATCH THE NEETCODE VIDEO MAYBE!!!!
         return backtrack1_PickOrNot(candidates, target);
@@ -54,12 +54,18 @@ public class Solution {
         backtrack1_PickOrNotHelper(candidates, idx+1, target, curSum+candidates[idx], subset, res);
         subset.RemoveAt(subset.Count-1);
 
-        //Skip repetition of this element (since the above loop takes care of that and we don't want to do it twice(or more)!!!!)
+        //**IMPORTANT PART:** SKIP ALL FUTURE INSTANCES OF THIS DIGIT (because the backtrack1Helper recursive call above will deal with those, 
+        // we simply use the next case to when NONE of this digit (from ANY index after current index) is used)
         //This loop in conjunction with recursion helps do the following:
-        //* Assume we start with array [1, 1, 1, 1, 2]
-        //* First, above case considers [1], then [1,1], then [1,1,1], then [1,1,1,1], then [1,1,1,1,2]
-        //* The loop below makes it so that for each of the above levels, respectively, we consider the following cases after not including current element:
-        //* i.e. [2], [1,2], [1,1,2], [1,1,1,2], [1,1,1,2], and then []
+        //* Assume we start with array [1, 2, 2, 3]
+        //* First, above case considers L: [1], LL: [1,2], LLL: [1,2,2], LLLL: [1,2,2,3]
+        //* The loop below makes it so that for each of the above levels/nodes, respectively, we consider the following cases after not including current element:
+        //* i.e. 
+        // R: [2], LR: [1, 3], LRL: [1,3], LRR: [1], .......
+        // LLR:[1, 2, 3], LLRL: ..., LLRR: ...
+        // LLLR: [1, 1, 1, 2], ......
+        //(watch Neetcode video for full chart)
+        // RRRR: [] //Here, idx+1 == nums.Length (because last element), so the loop is skipped, and upon further recursion, we get our empty set back.
         while((idx+1)<candidates.Length && candidates[idx]==candidates[(idx+1)])
             idx++;
 
