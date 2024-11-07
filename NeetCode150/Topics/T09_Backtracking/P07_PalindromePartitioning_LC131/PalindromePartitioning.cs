748fa83b548786893e8dd10bf65dfcdc3eb4e4dd
Noman <baba.noman@live.com>
2024-11-07 02:02:00 +0000

    A2M8 : NC150 : Bktrk : Palindrome Partitioning : LC131 : Submission 4 [Accepted]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T09_Backtracking/P07_PalindromePartitioning_LC131/PalindromePartitioning.cs
+++ NeetCode150/Topics/T09_Backtracking/P07_PalindromePartitioning_LC131/PalindromePartitioning.cs
@@ -7,6 +7,9 @@ using System.Text;
 namespace DSA.NeetCode150.Topics.T09_Backtracking.P07_PalindromePartitioning_LC131;
 
 public class Solution {
+    //**NOTE:** WATCH TakeUForward's L17. Palindrome Partitioning video to learn how this works. (4:00 to 14:00, can watch most of it on 2x).
+    //** {OR CHECK THE EXAMPLE RUN AT THE END OF Backtrack1's dfs funtion} **
+
     //HAD TO WATCH THE VIDEO TO DO IT! STILL DON'T FULLY UNDERSTAND
     //this helps: https://www.geeksforgeeks.org/given-a-string-print-all-possible-palindromic-partition/
     
@@ -46,7 +49,7 @@ public class Backtrack1
             return;
         }
 
-        for(int j = idx;j<s.Length;j++) //is every possible substring (starting at idx) a palindrome
+        for(int j = idx;j<s.Length;j++) //is every possible substring (starting at idx) a palindrome //Go through through all the substrings to find all the ones that can be in THIS partition. e.g. For aabb, on first level: a|abb, aa|bb. aabb| but NOTaab|b because the chosen partition doesn't create a palindrome on the left.
         {
             if(isPalindrome(s, idx, j))
             {
@@ -55,6 +58,13 @@ public class Backtrack1
                 part.RemoveAt(part.Count-1);
             }
         }
+
+        //Example run: ([*] means * is a valid, complete parition we can add to output.)
+        //For aabb, on first level: a|abb, aa|bb, but NOTaab|b ORaabb because the chosen partition doesn't create a palindrome on the left.
+        //On second level: a|a|bb, aa|b|b, [aa|bb|].
+        //On third level:  a|a|b|b, [a|a|bb|], [aa|b|b|]
+        //On fourth level: [a|a|b|b|].
+        //Therefore, the result = [a|a|b|b|, a|a|bb|, aa|b|b|, aa|bb|].
     }
 
     public bool isPalindrome(string s, int i, int j)
