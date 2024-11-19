c2b81e230a8bd67425e59b4c4c88e80fb16bd796
Noman <baba.noman@live.com>
2024-11-19 02:10:33 +0000

    A2M8 : NC150 : Tries : Word Search II : LC212 : Submission 7 [Accepted]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T10_Tries/P03_WordSearch2_LC212/WordSearchIi.cs
+++ NeetCode150/Topics/T10_Tries/P03_WordSearch2_LC212/WordSearchIi.cs
@@ -11,7 +11,7 @@ public class Solution {
         //Wasn't extremely hard, but very verbose (and the duplicates of words not being allowed was tricky).
 
         //Watch NeetCode video!! (especially to see how the refs based non-HashSet solution works!) 
-        //OHH maybe I could also just have set isWord to false for each node that I added to the list instead of switching to hashset
+        //OHHHH I could also just have set isWord to false for each node that I added to the list instead of switching to hashset
 
         //IMPORTANT!!! I DID NOT THINK ABOUT THE SAME WORD APPEARING MULTIPLE TIMES!! (good clarification question?)
 
