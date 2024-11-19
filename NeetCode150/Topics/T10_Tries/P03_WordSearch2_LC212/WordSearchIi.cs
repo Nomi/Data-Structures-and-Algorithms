fccdf3806fd0f24b51899a928f85177ecabee51b
Noman <baba.noman@live.com>
2024-11-19 02:13:59 +0000

    A2M8 : NC150 : Tries : Word Search II : LC212 : Submission 8 [Accepted]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T10_Tries/P03_WordSearch2_LC212/WordSearchIi.cs
+++ NeetCode150/Topics/T10_Tries/P03_WordSearch2_LC212/WordSearchIi.cs
@@ -11,11 +11,13 @@ public class Solution {
         //Wasn't extremely hard, but very verbose (and the duplicates of words not being allowed was tricky).
 
         //Watch NeetCode video!! (especially to see how the refs based non-HashSet solution works!) 
-        //OHHHH I could also just have set isWord to false for each node that I added to the list instead of switching to hashset
+        //OHHHH I could also just have set isWord to false for each node that I added to the list instead of switching to hashset 
+        //(though REFS based soln MIGHT be the best if it increases efficiency)
 
         //IMPORTANT!!! I DID NOT THINK ABOUT THE SAME WORD APPEARING MULTIPLE TIMES!! (good clarification question?)
 
-        return Attempt1.FindWords(board, words);
+        // return Attempt1.FindWords(board, words);
+        return Attempt1.FindWordsNonHashSet(board, words);
     }
 }
 
@@ -85,6 +87,62 @@ public static class Attempt1
         board[r][c] = curChar;
     }
 
+    public static List<string> FindWordsNonHashSet(char[][] board, string[] words) {
+        root = new TrieNode();
+        maxWordLen = 0;
+        
+        foreach(var word in words)
+        {
+            AddWord(word, root);
+        }
+
+        List<string> res = new(); //IMPORTANT!!! I DID NOT THINK ABOUT THE SAME WORD APPEARING MULTIPLE TIMES!! (good clarification question?)
+        List<char> wordSoFar = new(maxWordLen);
+
+        for(int r=0; r<board.Length;r++)
+        {
+            for(int c=0; c<board[0].Length;c++)
+            {
+                dfsNonHashSet(board, r, c, root, wordSoFar, res);
+            }
+        }
+
+        return res;
+    }
+
+    private static void dfsNonHashSet(char[][] board, int r, int c, TrieNode prevNode, List<char> wordSoFar, List<string> res)
+    {
+        // Console.WriteLine($"r:{r} / {board.Length-1}, c:{c} / {board[0].Length-1}");
+        if(r<0||r>=board.Length||c<0||c>=board[0].Length||board[r][c]==visited||wordSoFar.Count==maxWordLen) //im dumb cuz I spend so long debugging just to realize I was using r > length and c > length instead of >= !!!
+        {
+            // Console.WriteLine($"{r<0}||{r>=board.Length}||{c<0}||{c>=board[0].Length}");
+            return;
+        }
+        // Console.WriteLine($"--> {board[r][c]}, {wordSoFar.Count} / {maxWordLen}");
+
+        if(!prevNode.children.ContainsKey(board[r][c]))
+            return;
+
+        var curChar = board[r][c];
+        board[r][c] = visited;
+        wordSoFar.Add(curChar);
+
+        var curNode = prevNode.children[curChar];
+        // if(curNode.isWord) Console.WriteLine($"-----------------------------{string.Concat(wordSoFar)}");
+        if(curNode.isWord)
+        {
+            res.Add(string.Concat(wordSoFar));
+            curNode.isWord = false;
+        }
+
+        dfsNonHashSet(board, r-1, c, curNode, wordSoFar, res);    //I SPENT SOOO FUCKING LONG TO DEBUG THE IF CONDITIONS AT THE TOP THAT I DIDN'T REALIZE THE PROBLEM WAS I WAS PASSING ROOT INSTEAD OF CURRENT NODE BY MISTAKE WTFFF!!!
+        dfsNonHashSet(board, r+1, c, curNode, wordSoFar, res);
+        dfsNonHashSet(board, r, c-1, curNode, wordSoFar, res);
+        dfsNonHashSet(board, r, c+1, curNode, wordSoFar, res);
+
+        wordSoFar.RemoveAt(wordSoFar.Count-1);
+        board[r][c] = curChar;
+    }
 
     private static void AddWord(string word, TrieNode root)
     {
