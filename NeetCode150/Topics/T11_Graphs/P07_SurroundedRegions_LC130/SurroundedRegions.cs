e99fb11a747aa7035bc0ab217688d8f4d4910e40
Noman <baba.noman@live.com>
2024-11-21 22:29:10 +0000

    A2M8 : NC150 : Graphs : Surrounded Regions : LC130 : Submission 8 [Accepted]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T11_Graphs/P07_SurroundedRegions_LC130/SurroundedRegions.cs
+++ NeetCode150/Topics/T11_Graphs/P07_SurroundedRegions_LC130/SurroundedRegions.cs
@@ -29,6 +29,8 @@ public class Dfs1 : ISurroundedRegionsSolver
     // * COULDN'T COME UP WITH THE SOLUTION ON MY OWN (did think about trying to invert the problem but I did not invert it correctly enough)
     // * HAD TO WATCH NEETCODE'S VIDEO TO WRITE THIS!!!!
 
+    // I had initially CONCIOUSLY assumed that ROWS==COLS but I did comment next to the loop that I was assuming this.
+
     const char O = 'O';
     const char X = 'X';
     const char NOT_SURROUNDED = '#';
