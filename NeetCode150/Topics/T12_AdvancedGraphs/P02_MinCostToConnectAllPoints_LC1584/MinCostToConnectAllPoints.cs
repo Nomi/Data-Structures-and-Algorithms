f127a9119c8094f69bc2f067c5c4df2a13b2ee6f
Noman <baba.noman@live.com>
2024-11-27 23:25:45 +0000

    A2M8 : NC150 : AG : Min Cost to Connect All Points : LC1584 : Submission 1 [Compilation Error]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

---
+++ NeetCode150/Topics/T12_AdvancedGraphs/P02_MinCostToConnectAllPoints_LC1584/MinCostToConnectAllPoints.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DSA.NeetCode150.Topics.T12_AdvancedGraphs.P02_MinCostToConnectAllPoints_LC1584;
+
+public class Solution {
+    IMinCostToConnectPointsCalculator soln;
+    public int MinCostConnectPoints(int[][] points) {
+        //Basically shortest path in an undirected AND weighted graph problem, so I'm thinking Djikstra's algorithm (I remember a little bit about it from a video I watched a long time ago)
+        //OKAY, IT MIGHT NOT BE THE CORRECT APPROACH. AT LEAST Neetcodeio solns don't use it.
+        soln = new Djikstra_1();
+
+        return soln.MinCostConnectPoints(points);
+    }
+
+}
+
+
+public interface IMinCostToConnectPointsCalculator
+{
+    int MinCostConnectPoints(int[][] points);
+}
+
+public class Djikstra_1 : IMinCostToConnectPointsCalculator
+{
+    int Abs(int num) => (int) Math.Abs(num);
+    int Cost(int x1, int y1, int x2, int y2) => Abs(x1-x2) + Abs(y1-y2); //returns ManhattanCost
+
+    public int MinCostConnectPoints(int[][] points)
+    {
+        
+    }
+}
