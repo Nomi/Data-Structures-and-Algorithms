ba0e0b90b05a48d6b68dbeeada23bc8c484595e7
Noman <baba.noman@live.com>
2024-11-28 18:47:57 +0000

    A2M8 : NC150 : AG : Min Cost to Connect All Points : LC1584 : Submission 2 [Compilation Error]
    [NOTE: A2M8 <=> This is an automated commit (with same commit time as original submission time). Check the PR (*TO* main branch) for more details.]

--- NeetCode150/Topics/T12_AdvancedGraphs/P02_MinCostToConnectAllPoints_LC1584/MinCostToConnectAllPoints.cs
+++ NeetCode150/Topics/T12_AdvancedGraphs/P02_MinCostToConnectAllPoints_LC1584/MinCostToConnectAllPoints.cs
@@ -11,7 +11,14 @@ public class Solution {
     public int MinCostConnectPoints(int[][] points) {
         //Basically shortest path in an undirected AND weighted graph problem, so I'm thinking Djikstra's algorithm (I remember a little bit about it from a video I watched a long time ago)
         //OKAY, IT MIGHT NOT BE THE CORRECT APPROACH. AT LEAST Neetcodeio solns don't use it.
-        soln = new Djikstra_1();
+        
+        //OKAY, I WAS DUMB, DJIKSTRA IS LOWEST_COST PATH TO A NODE (or all nodes) IN DIRECTED and WEIGHTED GRAPHS 
+        //BUT IT DOESN'T ACCOUNT FOR HAVING TO GO THROUGH ALL NODES!!
+        //This problem is more like a Minimum Spanning Tree, meaning we can use the following algorithms:
+        //  * Prim's Algorithm
+        //  * Kruskal's Algorithm
+
+        soln = new PrimsAlgo_1();
 
         return soln.MinCostConnectPoints(points);
     }
@@ -24,13 +31,13 @@ public interface IMinCostToConnectPointsCalculator
     int MinCostConnectPoints(int[][] points);
 }
 
-public class Djikstra_1 : IMinCostToConnectPointsCalculator
+public class PrimsAlgo_1 : IMinCostToConnectPointsCalculator
 {
     int Abs(int num) => (int) Math.Abs(num);
     int Cost(int x1, int y1, int x2, int y2) => Abs(x1-x2) + Abs(y1-y2); //returns ManhattanCost
 
     public int MinCostConnectPoints(int[][] points)
     {
-        
+
     }
 }
